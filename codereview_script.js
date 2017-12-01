4b79ce5c5bbb85383b0e926bdd773a24d08ebb36
viktoria Herzer <viktoria.herzer@gmail.com>
2017-12-02 00:44:05 +0100

    finished layout, only java is missing now

---
+++ codereview_script.js
@@ -0,0 +1,12 @@
+amsterdam=[1500, 500, 1800, 700];
+london=[2500, 1500, 1000, 900];
+
+
+function avarage(amsterdam, london){
+	var amsterdam=[1500, 500, 1800, 700];
+	var london=[2500, 1500, 1000, 900];
+	var dam = amsterdam/4;
+	var don = london/4;
+	console.log(dam + don);
+}
+console.log(dam + don);
